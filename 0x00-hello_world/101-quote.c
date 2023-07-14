ce053432ca2ddbe139424e170a7a8cf17f3b51bb
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-14 16:35:47 -0700

    track and push script file for taks 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+const char message[] = and that piece of art is useful\"-Dora Korpar,2015-10-19
+syscall(SYS_write, 1, message, sizeof(message) - 1);
+return (1);
+}
