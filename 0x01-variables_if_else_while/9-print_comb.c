74ce2b7eea309f65d1e258bf7d2a6bb5b827df60
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-14 20:40:53 -0700

    fix script file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,14 @@
 
 int main(void)
 {
-	int x, y;
+	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		for (y = x; y < 10; y++)
-		{
-			putchar(x + '0');
-			putchar(',');
-			putchar(y + '0');
-			putchar(',');
-		}
+		putchar(x);
 	}
+
+	putchar(',');
 
 	return (0);
 }
