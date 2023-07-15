a3ec44b1d68d2fbdfbeac2cc9338248e21c0a980
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-14 20:56:53 -0700

    add a new script file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar('0' + x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
