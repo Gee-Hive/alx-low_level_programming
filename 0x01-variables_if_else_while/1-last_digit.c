6befbfae9024ab7de32bf2de59ecb408572f8c9b
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-14 18:12:10 -0700

    factor script file and output

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,23 +11,26 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int lastDigit = n % 10;
+	printf("Last digit of %d is ", n);
+
+	lastDigit = n % 10;
 
 	if (lastDigit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("%d and is greater than 5\n", lastDigit);
 	}
 	else if (lastDigit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+		printf("%d and is 0\n", lastDigit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+		printf("%d and is less than 6 and not 0\n", lastDigit);
 	}
 	return (0);
 }
