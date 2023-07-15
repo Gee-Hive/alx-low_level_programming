82c79f1832117e1332e15b1373541d39bfae66c0
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-14 20:14:25 -0700

    add new script file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = x; y < 10; y++)
+		{
+			putchar(x +'0');
+			putchar(',');
+			putchar(y + '0');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
