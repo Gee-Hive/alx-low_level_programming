116fbd74ce356cfeac06465f24ab900a2874df51
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-17 19:01:01 -0700

    modify new script file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,24 +11,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			if (i != 0)
-			{
-				putchar('0' + i / 10);
-				putchar('0' + i % 10);
-			}
-			else
-			{
-				putchar('0');
-				putchar('0');
-			}
+			putchar(i + '0');
 			putchar(' ');
-			putchar('0' + j / 10);
-			putchar('0' + j % 10);
-			if (i != 98 || j != 99)
+			putchar(j + '0');
+
+			if (!(i == 9 && j == 9))
 			{
 				putchar(',');
 				putchar(' ');
