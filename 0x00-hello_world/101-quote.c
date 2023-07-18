1029c1e064849a08d299fb7758a7deba91409020
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-17 18:29:37 -0700

    add new script file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-const char message[] = and that piece of art is useful\"-Dora Korpar,2015-10-19
-syscall(SYS_write, 1, message, sizeof(message) - 1);
-return (1);
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, message, sizeof(message) - 1);
+
+	return (1);
+
 }
