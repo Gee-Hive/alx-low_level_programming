ff4125625b3d3777e8f7fc464cf2c5eadb045a4c
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-18 18:36:33 -0700

    add a new file that print the last number of a digit passed into a func

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_last_digit - print the last digit of a number passed in
+ *
+ * @num: is the parameter or input value passed into the function
+ * Return: 1 (success)
+ */
+
+int print_last_digit(int num)
+{
+	int lastDigit = num % 10;
+
+	return (lastDigit);
+}
