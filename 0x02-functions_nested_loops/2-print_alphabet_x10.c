092f48b6b1dbd8d5f1080e5d4bc77725a6f84deb
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-18 17:03:45 -0700

    add new script

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int times;
+
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		while (times < 11)
+		{
+			_putchar(letters);
+			times++
+		}
+	}
+	
+	_putchar('\n');
+}	
