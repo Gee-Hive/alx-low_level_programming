2a634361a540b76a6cf0f00f13ca6ba5a625406e
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-18 19:07:21 -0700

    modify code block

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -4,14 +4,14 @@
  * print_last_digit - print the last digit of a number passed in
  *
  * @num: is the parameter or input value passed into the function
- * Return: 1 (success)
+ * Return: int (success)
  */
 
 int print_last_digit(int num)
 {
 	int lastDigit = num % 10;
 
-	int result = (lastDigit * 10) + lastDigit;
+	int result = lastDigit + 10 * lastDigit;
 
 	return (result);
 }
