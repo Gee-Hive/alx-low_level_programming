b35af86695b08e5911ee1cd932bdd9015e208870
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-18 17:55:11 -0700

    add new script file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha -  checks for alphabetic character either upper or lowercase
+ *
+ * @c: is the input or parameter passed into _isalpha function
+ * Return: 1 (if upper or lowercase)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' || c >= 'A' && c <= 'z' || c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
