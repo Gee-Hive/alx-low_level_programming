bd40f3a568cc45b574c3882a7c78e5ef68feeaf6
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-19 04:25:15 -0700

    add new file that adds two intergers and prints the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - adds two integer and returns the result
+ *
+ * @num: is the parameter or input passed in
+ * Return: int
+ */
+
+int add(int num, int num)
+{
+	int result = num + num;
+
+	return (result);
+}
