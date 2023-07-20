ad163c3f36d49aeaa0d534083f75e77372508791
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-20 06:43:56 -0700

    add new file that checks for digit characters

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include <stdio>
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit passed or not
+ * @c: is the input or parameter passed in
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
