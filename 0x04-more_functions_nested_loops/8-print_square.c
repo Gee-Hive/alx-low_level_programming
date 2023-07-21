9a665c711deb07f0bc729018a52ec2e54319fc20
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-20 18:59:42 -0700

    modify code line

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,15 +10,16 @@ void print_square(int size)
 {
 	int x, y;
 
+	if (size == 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
 	for (x = 0; x < size; x++)
 	{
 		for (y = 0; y < size; y++)
 		{
-			if (size == 0)
-			{
-				_putchar('\n');
-				return;
-			}
 			_putchar('#');
 		}
 
