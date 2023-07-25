126573ebd287dd07cced54203da24a16dcf95ea5
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-24 17:05:28 -0700

    add an fnct that print a string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string -  reverses a string
+ *
+ * @s: input param
+ */
+
+void rev_string(char *s)
+{
+	int length = strlen(s);
+	int a, b;
+
+	for (a = 0, b = length - 1; a < b; a++, b--)
+	{
+		char t = s[a];
+		s[a] = s[b];
+		s[b] = t;
+	}
+}
