e536f9de7f14694e2c4fc6c6da80b0d275348f76
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-26 20:40:11 -0700

    add new file that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: input param
+ * Return: char
+ */
+
+char *leet(char *str)
+{
+	int a, b;
+
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (str[a] == s1[b])
+			{
+				str[a] = s2[b];
+			}
+		}
+	}
+	return (str);
+}
+
