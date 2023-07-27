7ad31251419ce150e0846401895ea7bf03f5b424
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-26 20:11:05 -0700

    modify code block

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,23 +10,29 @@
 
 char *cap_string(char *str)
 {
-	int cap_next = 1;
-	char *ptr = str;
+	int i = 0;
 
-	while (*ptr)
+	while (str[i])
 	{
-		if (isspace(*ptr) || ispunct(*ptr))
-		{
-			cap_next = 1;
-		}
-		else if (cap_next)
-		{
-			*ptr = toupper(*ptr);
-			cap_next = 0;
-		}
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
 
-		ptr++;
+			i++;
+		if (str[i] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
+			str[i] -= 32;
+		i++;
 	}
-
 	return (str);
 }
