b975880784e8d107845ad8be94a0a9e55192cc5e
Ogunleye <gbengaogunleye34@gmail.com>
2023-07-26 18:56:21 -0700

    add new file that changes from lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: is the pointer to the index character of the str
+ * Return: char
+ */
+
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr)
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr = *ptr - 'a' + 'A';
+		}
+		ptr++;
+	}
+	return (str);
+}
