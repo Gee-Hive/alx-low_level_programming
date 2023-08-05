425b3300c1123028a79434f91ff5ffb81b2f2697
Ogunleye <gbengaogunleye34@gmail.com>
2023-08-04 20:58:15 -0700

    add new func that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector (arrays with argument)
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc; /*marke paramter as unused*/
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0x0A. C - argc, argv - this project is all about arguments to main in C language
