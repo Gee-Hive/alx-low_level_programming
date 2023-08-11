29b6221c88f358005b8762d89113cde2d3b3de91
Ogunleye <gbengaogunleye34@gmail.com>
2023-08-10 20:19:51 -0700

    add new function file that allocate memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: input param
+ * Return: a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This Project is all about more Malloc which deals with dynamic memory allocation
