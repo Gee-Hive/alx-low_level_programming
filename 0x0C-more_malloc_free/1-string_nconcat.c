693172e76b5bb8ae670337e61c5838089cc4f674
Ogunleye <gbengaogunleye34@gmail.com>
2023-08-10 21:37:44 -0700

    modify code bloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,10 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2[j] != '\0')
 	{
 		j++;
-	}
-	if (n >= j)
-		n = j;
-	j = 0;	
+	}	
 	p = malloc(sizeof(char) * (i + n + 1));
 	if (p == NULL)
 		return ('\0');
