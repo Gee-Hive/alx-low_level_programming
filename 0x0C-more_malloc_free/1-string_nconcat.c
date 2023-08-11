c2fbfa2fab12d48773855eb32c52e2ff64cfb8c6
Ogunleye <gbengaogunleye34@gmail.com>
2023-08-10 21:29:26 -0700

    add new func file that adds to strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *string_nconcat - concatenates two strings
+ * @s1: first input param
+ * @s2: second input param
+ * @n: third input param
+ * Return: a char pointer
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int i = 0, j = 0, size;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+	
+	p = malloc(sizeof(char) * (i + n + 1));
+	if (p == NULL)
+		return ('\0');
+	for (size = 0; size < i; size++)
+	{
+		p[size] = s1[size];
+	}
+	for (; size < (i + n); size++)
+	{
+		p[size] = s2[size - i];
+	}
+	p[size] = '\0';
+	return (p);
+}
