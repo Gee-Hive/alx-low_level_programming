ae148c5deedd1857ae743519f764b90fbf27e463
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2023-08-15 07:58:40 -0700

    add a newheader file that defines a data struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,4 @@
+This Directory is all about typedef and structures in C
+
+What are structures, when, why and how to use them
+How to use typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - defines a new structure of type dog
+ * @name: first field
+ * @age: second field
+ * @owner: third field
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
