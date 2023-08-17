b57e799b0af368f32b2ecc4b58b200567b732252
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2023-08-16 20:44:29 -0700

    add new function file that searches for an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - searches an integer
+ * @array: first value
+ * @size: function
+ * @cmp: function
+ *
+ * Return: Value
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int val = 0, i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		val = cmp(array[i]);
+
+		if (val != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
