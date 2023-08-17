f7e36154fabd1f0d19f200b3fb1bb5127ed9adda
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2023-08-17 10:29:40 -0700

    modify code block

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int val = 0, i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
-
 	for (i = 0; i < size; i++)
 	{
 		val = cmp(array[i]);
