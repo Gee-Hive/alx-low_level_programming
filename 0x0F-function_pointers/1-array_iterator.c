8bfe908c03bbed4f32e15614b36adf595f0cf91b
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2023-08-16 20:21:13 -0700

    add new functon file that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - execute a function given an array
+ * @array: first input param
+ * @size: function
+ * @action: function
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
