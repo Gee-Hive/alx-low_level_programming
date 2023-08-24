3cfb203c51a86cd1e99f1de5cc926546dd8b2356
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2023-08-24 08:23:18 -0700

    modify list.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef lists.h
-#define lists.h
+#ifndef LIST_H
+#define LIST_H
 
 /**
  * struct list_s - singly linked list
@@ -19,3 +19,4 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 
+#endif
