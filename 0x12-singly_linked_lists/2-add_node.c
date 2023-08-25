798b73a0aa3375047f4315129b10a6e70eb73fa4
Ogunleye Gbenga <gbengaogunleye34@gmail.com>
2023-08-24 19:32:00 -0700

    implement func file that  adds a new node at the beginning of a list_t list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - add new node
+ * @head: pointer of node
+ * @str: string with info
+ *
+ * Return: address of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	
+	if (new_node == NULL)
+	{
+		return ('\0');
+	}
+
+	/* Duplicate the input string using strdup */
+	new_node->str = strdup(str);
+	
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return ('\0');
+	}
+
+	new_node->next = *head;/*Link the new node to the current head*/
+
+	*head = new_node; /* Update the head pointer to point to the new node */
+
+	return (new_node);
+}
